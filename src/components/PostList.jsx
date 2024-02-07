356619591b8337bfed8b954778f1e22bd42389a0
Abasalat <abasalat.bscsf21@iba-suk.edu.pk>
2024-02-07 12:13:57 +0500

    Add Login spinner

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,26 +1,33 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostListData } from "../store/post-list-store";
 import Welcome from "./Welcome";
+import LoadingSpinner from "./LoadingSpinner";
 
 const PostList = () => {
   const { postList, addInitialPost } = useContext(PostListData);
-
-  const handleGetPostsClick = () => {
-    fetch("https://dummyjson.com/posts")
+  const [fetching, setFetching] = useState(false);
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+    setFetching(true);
+    fetch("https://dummyjson.com/posts", { signal })
       .then((res) => res.json())
       .then((data) => {
         addInitialPost(data.posts);
+        setFetching(false);
       });
-  };
+    return () => {
+      console.log("Cleaning up UseEffect");
+      controller.abort();
+    };
+  }, []);
+
   return (
     <>
-      {postList.length === 0 && (
-        <Welcome onGetPostsClick={handleGetPostsClick} />
-      )}
-      {postList.map((post) => (
-        <Post key={post.id} post={post} />
-      ))}
+      {fetching && <LoadingSpinner />}
+      {!fetching && postList.length === 0 && <Welcome />}
+      {!fetching && postList.map((post) => <Post key={post.id} post={post} />)}
     </>
   );
 };
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,18 +1,11 @@
 import React from "react";
 
-const Welcome = ({ onGetPostsClick }) => {
+const Welcome = () => {
   return (
     <center className="welcome-message">
       <marquee scrollamount="10">
         <h1>There is no post available yet</h1>
       </marquee>
-      <button
-        type="button"
-        className="btn btn-primary"
-        onClick={onGetPostsClick}
-      >
-        Get Post From Server
-      </button>
     </center>
   );
 };
